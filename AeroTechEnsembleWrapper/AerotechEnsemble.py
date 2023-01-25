154a0e35749c86dafec9842ffef5c30d3a833cc6
Tinyblack <tinyblack.zhao@gmail.com>
2023-01-25 18:43:14 +0000

    Update

--- AeroTechEnsembleWrapper/AerotechEnsemble.py
+++ AeroTechEnsembleWrapper/AerotechEnsemble.py
@@ -808,6 +808,51 @@ class Axis():
         self.status=AxisDiagPacket()
         self.axisNET=None
         
+class AerotechCommonCollections():
+    class INamedCollection(Sequence):
+        def __init__(self,INamed):
+            self.INamed=INamed
+            super().__init__()
+
+        def __getitem__(self, i):
+            return AerotechEnsemble.Controller(self.INamed[i])
+        
+        def __len__(self):
+            return len(self.INamed)
+
+    class INamedConstantCollection(Sequence):
+        def __init__(self,INamedConstant):
+            self.INamedConstant=INamedConstant
+            super().__init__()
+
+        def __getitem__(self, i):
+            return AerotechEnsemble.Controller(self.INamedConstant[i])
+        
+        def __len__(self):
+            return len(self.INamedConstant)
+
+    class INamedMaskableConstantCollection(Sequence):
+        def __init__(self,INamedMaskableConstant):
+            self.INamedMaskableConstant=INamedMaskableConstant
+            super().__init__()
+
+        def __getitem__(self, i):
+            return AerotechEnsemble.Controller(self.INamedMaskableConstant[i])
+        
+        def __len__(self):
+            return len(self.INamedMaskableConstant)
+
+    class INamedMaskedConstantCollection(Sequence):
+        def __init__(self,INamedMaskedConstant):
+            self.INamedMaskedConstant=INamedMaskedConstant
+            super().__init__()
+
+        def __getitem__(self, i):
+            return AerotechEnsemble.Controller(self.INamedMaskedConstant[i])
+        
+        def __len__(self):
+            return len(self.INamedMaskedConstant)
+    
 class AerotechEnsemble():
     _Controller=None
     class AxisMask():
