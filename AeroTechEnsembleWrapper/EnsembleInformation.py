ed7bcd67263700eb79e98cdb214a658e7a8bad06
Tinyblack <tinyblack.zhao@gmail.com>
2023-01-27 17:17:19 +0000

    EnsembleInformation Done

--- AeroTechEnsembleWrapper/EnsembleInformation.py
+++ AeroTechEnsembleWrapper/EnsembleInformation.py
@@ -8,33 +8,18 @@ sys.path.extend(glob.glob(f'{pathlib.Path(__file__).parents[0].resolve()}/*/**/'
 
 import clr
 clr.AddReference('System')
-from System import String, Char, Int32, IntPtr,Text, UInt32,Decimal,Double
-
-from copy import deepcopy
-from win32api import GetFileVersionInfo, LOWORD, HIWORD
-
-from GlobalLogger import GlobalLogger
+import System
 
 from enum import Enum
-
-import time
+from aenum import extend_enum
+from typing import Union,type
 
 from multimethod import multimethod
 
-import Common
 import CommonCollections
-import EnsembleCommands 
 import EnsembleCommunication 
-import EnsembleConfiguration  
-import EnsembleDataCollection
-import EnsembleExceptions
-import EnsembleFileSystem
-import EnsembleFirmware
-import EnsembleInformation
-import EnsembleParameters
 import EnsembleStatus
-import EnsembleTasks
-import EnsembleTasksDebug
+
 
 DEFAULT_DLL_PATH:str=os.path.join(os.path.join(os.path.dirname(__file__),'Aerotech_DotNet_dll'),'')
 DEFAULT_DLL_NAME:str='Aerotech.Ensemble'
@@ -42,50 +27,242 @@ if DEFAULT_DLL_PATH.upper() not in [path.upper() for path in sys.path]:
     sys.path.extend(DEFAULT_DLL_PATH)
 try:
     clr.AddReference(DEFAULT_DLL_NAME)
-    import Aerotech.Ensemble as AerotechEnsembleNET
     import Aerotech.Ensemble.Information as AerotechEnsembleInformationNET
 except:
     raise RuntimeError
 
+# ! DONE
+
+class AxisInfo():
+    _AxisInfoNET=None
+    def __init__(self,AxisInfoNET=AerotechEnsembleInformationNET.AxisInfo):
+        self._AxisInfoNET=AxisInfoNET
+        
+    @property        
+    def AxisType(self): # The type of axis 
+        return ComponentType[self._AxisInfoNET.AxisType.ToString]
+
+    @property        
+    def BusVoltage(self): # The bus voltage
+        return int(self._AxisInfoNET.BusVoltage)
+
+    @property        
+    def CommandOutputType(self): # The type of output an axis generates.
+        return CommandOutputType[self._AxisInfoNET.CommandOutputType.ToString]
+
+    @property        
+    def Drive(self): # Returns information about the physical drive on which the axis resides.
+        return DriveInformation(self._AxisInfoNET.Drive)
+
+    @property        
+    def FlashConfigStatus(self): # Information about FlashConfig status.
+        return FlashConfigStatus(self._AxisInfoNET.FlashConfigStatus)
+
+    @property        
+    def IOBoardPresent(self): # If the IO board is connected. 
+        return self._AxisInfoNET.IOBoardPresent
+
+    @property        
+    def LEDToggle(self): # Whether the LED is brinking (toggling) on the given Axis 
+        return self._AxisInfoNET.LEDToggle
+
+    @property        
+    def MaximumAmperage(self): # The axis maximum amperage 
+        return float(self._AxisInfoNET.MaximumAmperage)
+
+    @property        
+    def MaximumVoltage(self): # The axis maximum voltage.
+        return float(self._AxisInfoNET.MaximumVoltage)
+
+    @property        
+    def Name(self): # The axis name.
+        return self._AxisInfoNET.Name
+
+    @property        
+    def Number(self): # The axis number 
+        return self._AxisInfoNET.Number
+ 
+class CommandOutputType(Enum):
+    Current=AerotechEnsembleInformationNET.ComponentOutputType.Current  # Current output is generated.
+    Voltage=AerotechEnsembleInformationNET.ComponentOutputType.Voltage  # Voltage output is generated. 
+extend_enum(CommandOutputType,'None',getattr(AerotechEnsembleInformationNET.CommandOutputType,'None'))
+
 class ComponentType(Enum):
     CP=AerotechEnsembleInformationNET.ComponentType.CP # Compact pulse width modulation
- 
     MP=AerotechEnsembleInformationNET.ComponentType.MP # Micro pulse width modulation
-    
     Control=AerotechEnsembleInformationNET.ComponentType.Control # Ensemble control board
-    
     CL=AerotechEnsembleInformationNET.ComponentType.CL  # Compact Linear
-    
     HPE=AerotechEnsembleInformationNET.ComponentType.HPE  # High performance pulse width modulation enhanced 
-    
     HLE=AerotechEnsembleInformationNET.ComponentType.HLE  # High performance linear enhanced 
-    
     ML=AerotechEnsembleInformationNET.ComponentType.ML  # Micro pulse linear
-    
     PMT=AerotechEnsembleInformationNET.ComponentType.PMT  # Obsolete
-    
     Lab=AerotechEnsembleInformationNET.ComponentType.Lab  # Ensemble Lab controller
-    
     QLab=AerotechEnsembleInformationNET.ComponentType.QLab  # Ensemble QLab controller
-    
     QDe=AerotechEnsembleInformationNET.ComponentType.QDe  # High Performance Single Axis Piezo Drive
-    
     QL=AerotechEnsembleInformationNET.ComponentType.QL  # Single Axis Piezo Drive
-    
     QLe=AerotechEnsembleInformationNET.ComponentType.QLe  # High Performance Single Axis Piezo Drive 
     
-class ControllerVersion ():
+class ControllerInformation():
+    _ControllerInformationNET=None
+    def __init__(self,ControllerInformationNET=AerotechEnsembleInformationNET.ControllerInformation):
+        self._ControllerInformationNET=ControllerInformationNET
+        
+    @property
+    def Axes(self): # Provides access to information about axes 
+        return CommonCollections.INamedConstantCollection(self._ControllerInformationNET.Axes,AxisInfo)
+
+    @property
+    def AxisMismatch(self): # Indicates a configuration error on the controller 
+        return EnsembleCommunication.AxisMismatch[self._ControllerInformationNET.AxisMismatch.ToString()]
+
+    @property
+    def CommunicationType(self): # The communication type that is being used
+        return EnsembleCommunication.CommunicationType[self._ControllerInformationNET.CommunicationType.ToString()]
+
+    @property
+    def Drives(self): # Provides information about connected drives.
+        return DriveInformation(self._ControllerInformationNET.Drives)
+
+    @property
+    def Initialization(self): # Provies information about initialization
+        return InitializationInformation(self._ControllerInformationNET.Initialization)
+
+    @property
+    def IsEpaq(self): # Whether the controller is an Epaq 
+        return self._ControllerInformationNET.IsEpaq
+
+    @property
+    def MasterType(self): # Specifies the component type of the controller 
+        return ComponentType[self._ControllerInformationNET.MasterType.ToString()]
+
+    @property
+    def Name(self): # The name of the controller
+        return self._ControllerInformationNET.Name
+
+    @property
+    def Version(self): # Specifies the versions of the controller  
+        return ControllerVersion(self._ControllerInformationNET.Version)
+
+class ControllerVersion():
+    _ControllerVersionNET=None
+    def __init__(self,ControllerVersionNET=AerotechEnsembleInformationNET.ControllerVersion):
+        self._ControllerVersionNET=ControllerVersionNET
 
     @property
     def FirmwareVersion(self):  #Specifies the firmware version
-        return AerotechEnsembleInformationNET.ControllerVersion.FirmwareVersion.ToString()
+        return self._ControllerVersionNET.FirmwareVersion.ToString()
     
     @property
     def FPGAVersion(self):  #Specifies the FPGA version 
-        return AerotechEnsembleInformationNET.ControllerVersion.FPGAVersion
+        return self._ControllerVersionNET.FPGAVersion
     
     @property
     def HardwareVersion(self):  #Specifies the hardware version 
-        return AerotechEnsembleInformationNET.ControllerVersion.HardwareVersion
+        return self._ControllerVersionNET.HardwareVersion
+    
+class DriveInformation():
+    _DriveInformationNET=None
+    def __init__(self,DriveInformationNET=AerotechEnsembleInformationNET.DriveInformation):
+        self._DriveInformationNET=DriveInformationNET
+        
+    @property     
+    def Axes(self): # Returns information about the axes that are present on the drive. 
+        return CommonCollections.INamedConstantCollection(self._DriveInformationNET.Axes,AxisInfo)
+
+    @property  
+    def IOBoardPresent(self): # If the IO board is connected.
+        return self._DriveInformationNET.IOBoardPresent
+        
+    @property  
+    def Type(self): # What type the drive is
+        return ComponentType[self._DriveInformationNET.Type.ToString()]
+        
+    @property  
+    def Version(self): # Specifies the version of the drive. 
+        return ControllerVersion(self._DriveInformationNET.Version)
+
+class EnumInformation():
+    _EnumInformationNET=None
+    def __init__(self,EnumInformationNET=AerotechEnsembleInformationNET.EnumInformation):
+        self._EnumInformationNET=EnumInformationNET
+        
+    def GetEnumName(self,enumType:System.Type): # Get the string representation of an enumeration.
+        return self._EnumInformationNET.GetEnumName
+    
+    def GetValueName(self,enumValue:System.Enum):  # Get the string representation of a value.
+        return self._EnumInformationNET.GetValueName
+    
+    def GetValueNames(self,enumType:System.Type):  # Get the string representations of values of an enumeration.
+        return self._EnumInformationNET.GetValueNames
+ 
+class FlashConfigStatus():
+    _FlashConfigStatusNET=None
+    def __init__(self,FlashConfigStatusNET:Union[AerotechEnsembleInformationNET.FlashConfigStatus,int]=None):
+        if type(FlashConfigStatusNET) in [type(int),type(None)]:
+            if type(FlashConfigStatusNET) in [type(int)]:
+                self._FlashConfigStatusNET=AerotechEnsembleInformationNET.FlashConfigStatus(FlashConfigStatusNET)
+            if  type(FlashConfigStatusNET) in [type(None)]:
+                self._FlashConfigStatusNET=AerotechEnsembleInformationNET.FlashConfigStatus()
+        else:
+            self._FlashConfigStatusNET=FlashConfigStatusNET
+            
+    @property    
+    def ActiveBits(self):  # Returns a list of the active bit names.
+        # TODO Deal with 'ReadOnlyCollection'
+        return str(self._FlashConfigStatusNET.ActiveBits)
+    
+    @property
+    def BitHelpLinks(self):  # Returns a dictionary of bit value names (keys) and the associated help file link (values) 
+        # TODO Deal with 'IDictionary'
+        return self._FlashConfigStatusNET.BitHelpLinks
+    
+    @property
+    def BitValues(self):  # Returns a listing of the bit names and their corresponding values 
+        # TODO Deal with 'IDictionary'
+        return self._FlashConfigStatusNET.BitValues
+    
+    @property
+    def DataValid(self):  # FlashConfig data is valid.
+        return self._FlashConfigStatusNET.DataValid
+
+    @property
+    def MaskValue(self):  #   The underlying mask value 
+        return self._FlashConfigStatusNET.MaskValue
+        
+    #None  If all the other properties are not set (false) 
+    
+    @property
+    def SerialMismatch(self):  #   Connected Stage serial number does not match expected Stage serial number.
+        return self._FlashConfigStatusNET.SerialMismatch
+    
+    @property
+    def Supported(self):  #   FlashConfig memory is present; feature is supported.
+        return self._FlashConfigStatusNET.Supported
+        
+    @multimethod
+    def ToString(self):  # Converts to a string representation 
+        return self._FlashConfigStatusNET.ToString()
+    
+    @multimethod
+    def ToString(self,userReadableFormat:bool):  #  Converts to a string representation 
+        return self._FlashConfigStatusNET.ToString(userReadableFormat)
+    
+    @property
+    def ValueNames(self):  # Returns a mapping of values to their human readable form.  
+        # TODO Deal with 'IDictionary'
+        return self._FlashConfigStatusNET.ValueNames
+
+class InitializationInformation():
+    _InitializationInformationNET=None
+    def __init__(self,InitializationInformationNET=AerotechEnsembleInformationNET.InitializationInformation):
+        self._InitializationInformationNET=InitializationInformationNET
+    
+    @property
+    def Warning(self):
+        return EnsembleStatus.ErrorInformation(self._InitializationInformationNET.Warning)
+    
+
+
+
  
 
--- AeroTechEnsembleWrapper/EnsembleStatus.py
+++ AeroTechEnsembleWrapper/EnsembleStatus.py
@@ -1,4 +1,315 @@
+import os
+import pathlib
+import sys
+import glob
+
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.extend(glob.glob(f'{pathlib.Path(__file__).parents[0].resolve()}/*/**/', recursive=True))
+
+import clr
+clr.AddReference('System')
+from System.ComponentModel import ProgressChangedEventHandler
+
+from multimethod import multimethod
+from enum import Enum
+from aenum import extend_enum
+
+import Ensemble
+import EnsembleTasksDebug
+import EnsembleStatus
+import EnsembleFileSystem
+
+DEFAULT_DLL_PATH:str=os.path.join(os.path.join(os.path.dirname(__file__),'Aerotech_DotNet_dll'),'')
+DEFAULT_DLL_NAME:str='Aerotech.Ensemble'
+if DEFAULT_DLL_PATH.upper() not in [path.upper() for path in sys.path]:
+    sys.path.extend(DEFAULT_DLL_PATH)
+try:
+    clr.AddReference(DEFAULT_DLL_NAME)
+    import Aerotech.Ensemble.Status as AerotechEnsembleStatusNET
+except:
+    raise RuntimeError
+
+
 class AxisDiagPacket():
+    _AxisDiagPacketNET=None
+    def __init__(self,AxisDiagPacketNET=AerotechEnsembleStatusNET.AxisDiagPacket):
+        self._AxisDiagPacketNET=AxisDiagPacketNET
+        
+    @property
+    def AbsoluteFeedback(self):  # The absolute feedback source position 
+        return self._AxisDiagPacketNET.AbsoluteFeedback
+
+    @property
+    def AccelerationCommand(self):  # The acceleration command 
+        return self._AxisDiagPacketNET.AccelerationCommand
+
+    @property
+    def AccelerationCommandCounts(self):  # The acceleration command, in counts 
+        return self._AxisDiagPacketNET.AccelerationCommandCounts
+
+    @property
+    def AccelerationError(self):  # The acceleration error, in user units 
+        return self._AxisDiagPacketNET.AccelerationError
+
+    @property
+    def AccelerationErrorCounts(self):  # The acceleration error, in counts 
+        return self._AxisDiagPacketNET.AccelerationErrorCounts
+
+    @property
+    def AccelerationFeedback(self):  # The acceleration Feedback 
+        return self._AxisDiagPacketNET.AccelerationFeedback
+
+    @property
+    def AccelerationFeedbackCounts(self):  # The acceleration Feedback, in counts 
+        return self._AxisDiagPacketNET.AccelerationFeedbackCounts
+
+    @property
+    def AmplifierTemperature(self):  # The amplifier temperature 
+        return self._AxisDiagPacketNET.AmplifierTemperature
+
+    @property
+    def AnalogInput0(self):  # Analog Input #0 
+        return self._AxisDiagPacketNET.AnalogInput0
+
+    @property
+    def AnalogInput1(self):  # Analog Input #1 
+        return self._AxisDiagPacketNET.AnalogInput1
+
+    @property
+    def AnalogInput2(self):  # Analog Input #2, available on certain hardware types only 
+        return self._AxisDiagPacketNET.AnalogInput2
+
+    @property
+    def AnalogInput3(self):  # Analog Input #3, available on certain hardware types only 
+        return self._AxisDiagPacketNET.AnalogInput3
+
+    @property
+    def AnalogOutput0(self):  # Analog Output #0 
+        return self._AxisDiagPacketNET.AnalogOutput0
+
+    @property
+    def AnalogOutput1(self):  # Analog Output #1 
+        return self._AxisDiagPacketNET.AnalogOutput1
+
+    @property
+    def AnalogOutput2(self):  # Analog Output #2, available on certain hardware types only 
+        return self._AxisDiagPacketNET.AnalogOutput2
+
+    @property
+    def AnalogOutput3(self):  # Analog Output #3, available on certain hardware types only 
+        return self._AxisDiagPacketNET.AnalogOutput3
+
+    @property
+    def AnalogOutput4(self):  # Analog Output #4, available on certain hardware types only 
+        return self._AxisDiagPacketNET.AnalogOutput4
+
+    @property
+    def AxisFault(self):  # Axis fault 
+        return AxisFault(self._AxisDiagPacketNET.AxisFault)
+
+    @property
+    def AxisName(self):  # The name of the axis for which this packet is for 
+        return self._AxisDiagPacketNET.AxisName
+
+    @property
+    def AxisStatus(self):  # Axis status 
+        return AxisStatus(self._AxisDiagPacketNET.AxisStatus)
+
+    @property
+    def CurrentCommand(self):  # Current command 
+        return self._AxisDiagPacketNET.CurrentCommand
+
+    @property
+    def CurrentError(self):  # Current error 
+        return self._AxisDiagPacketNET.CurrentError
+
+    @property
+    def CurrentFeedback(self):  # Current feedback 
+        return self._AxisDiagPacketNET.CurrentFeedback
+
+    @property
+    def DigitalInput0(self):  # Digital Input #0 
+        return self._AxisDiagPacketNET.DigitalInput0
+
+    @property
+    def DigitalInput1(self):  # Digital Input #1 
+        return self._AxisDiagPacketNET.DigitalInput1
+
+    @property
+    def DigitalInput2(self):  # Digital Input #2 
+        return self._AxisDiagPacketNET.DigitalInput2
+
+    @property
+    def DigitalOutput0(self):  # Digital Output #0 
+        return self._AxisDiagPacketNET.DigitalOutput0
+
+    @property
+    def DigitalOutput1(self):  # Digital Output #1 
+        return self._AxisDiagPacketNET.DigitalOutput1
+
+    @property
+    def DigitalOutput2(self):  # Digital Output #2 
+        return self._AxisDiagPacketNET.DigitalOutput2
+
+    @property
+    def PiezoVoltageCommand(self):  # The Voltage Command for Piezo stages.
+        return self._AxisDiagPacketNET.PiezoVoltageCommand
+
+    @property
+    def PiezoVoltageFeedback(self):  # The Voltage Feedback for Piezo stages.
+        return self._AxisDiagPacketNET.PiezoVoltageFeedback
+
+    @property
+    def PositionCommand(self):  # The position command, in user units 
+        return self._AxisDiagPacketNET.PositionCommand
+
+    @property
+    def PositionCommandCounts(self):  # The position command, in counts 
+        return self._AxisDiagPacketNET.PositionCommandCounts
+
+    @property
+    def PositionError(self):  # The position error, in user units 
+        return self._AxisDiagPacketNET.PositionError
+
+    @property
+    def PositionErrorCounts(self):  # The position error, in counts 
+        return self._AxisDiagPacketNET.PositionErrorCounts
+
+    @property
+    def PositionFeedback(self):  # The position feedback, in user units 
+        return self._AxisDiagPacketNET.PositionFeedback
+
+    @property
+    def PositionFeedbackAuxiliary(self):  # The position feedback auxiliary 
+        return self._AxisDiagPacketNET.PositionFeedbackAuxiliary
+
+    @property
+    def PositionFeedbackCounts(self):  # The position feedback, in counts 
+        return self._AxisDiagPacketNET.PositionFeedbackCounts
+
+    @property
+    def ProgramPositionCommand(self):  # The program position command, in user units 
+        return self._AxisDiagPacketNET.ProgramPositionCommand
+
+    @property
+    def ProgramPositionCommandCounts(self):  # The program position command, in counts 
+        return self._AxisDiagPacketNET.ProgramPositionCommandCounts
+
+    @property
+    def ProgramPositionError(self):  # The program position error, in user units 
+        return self._AxisDiagPacketNET.ProgramPositionError
+
+    @property
+    def ProgramPositionErrorCounts(self):  # The program position error, in counts 
+        return self._AxisDiagPacketNET.ProgramPositionErrorCounts
+
+    @property
+    def ProgramPositionFeedback(self):  # The program position feedback, in user units 
+        return self._AxisDiagPacketNET.ProgramPositionFeedback
+
+    @property
+    def ProgramPositionFeedbackCounts(self):  # The program position feedback, in counts 
+        return self._AxisDiagPacketNET.ProgramPositionFeedbackCounts
+
+    @property
+    def VelocityCommand(self):  # The velocity command, in user units 
+        return self._AxisDiagPacketNET.AelocityCommand
+
+    @property
+    def VelocityCommandCounts(self):  # The velocity command, in counts 
+        return self._AxisDiagPacketNET.VelocityCommandCounts
+
+    @property
+    def VelocityError(self):  # The velocity error, in user units 
+        return self._AxisDiagPacketNET.VelocityError
+
+    @property
+    def VelocityErrorCounts(self):  # The velocity error, in counts 
+        return self._AxisDiagPacketNET.VelocityErrorCounts
+
+    @property
+    def VelocityFeedback(self):  # The velocity feedback, in user units 
+        return self._AxisDiagPacketNET.VelocityFeedback
+
+    @property
+    def VelocityFeedbackCounts(self):  # The velocity feedback, in counts  
+        return self._AxisDiagPacketNET.VelocityFeedbackCounts
+ 
+class AxisFault():  # Represents the faults of an axis
+    _AxisFaultNET=None
+    def __init__(self,AxisFaultNET=AerotechEnsembleStatusNET.AxisFault):
+        self._AxisFaultNET=AxisFaultNET
+ 
+class AxisStatus():  # Represents an axis status
+    _AxisStatusNET=None
+    def __init__(self,AxisStatusNET=AerotechEnsembleStatusNET.AxisStatus):
+        self._AxisStatusNET=AxisStatusNET
+ 
+class CallbacksPoller():  # Allows to poll for callback information from a controller in the background 
+ 
+class ControlCenter():  # Retrieves diagnostic data, callbacks, and task states in the background from a controller 
+ 
+class ControllerDiagPacket():  # The diagnostic packet of the controller 
+
+class ControllerEventArgs():  # The base class for classes containing event data related to controllers. 
+ 
+class DebugFlags():  # Represents the debug flags on the controller
+ 
+class DiagPacketPoller():  # Allows to poll for diagnostic information from a controller in the background 
+ 
+class ErrorEventArgs():  # Provides data for the error events 
+  
+class ErrorInformation(): # Provides error information.
+    _ErrorInformationNET=None
+    
+    def __init__(self,ErrorInformationNET=AerotechEnsembleStatusNET.ErrorInformation):
+        self._ErrorInformationNET=ErrorInformationNET
+        
+    @property
+    def Description(self): # The description of the error.
+        return self._ErrorInformationNET.Description
+    
+    @property
+    def ErrorCode(self):   # The error code identifier.
+        return self._ErrorInformationNET.ErrorCode
+
+    @property
+    def HelpKey(self):   # Specifies the key to look for in the help file for a more detailed description.
+        return self._ErrorInformationNET.HelpKey
+    
+    @property
+    def HelpLink(self):   # Gets the link to the help associated with this error.
+        return self._ErrorInformationNET.HelpLink
+
+    def ToString(self)  # Returns a string representation of this class.
+        return self._ErrorInformationNET.ToString()
+
+        
+ 
+class InputBoxCallbackEventArgs(): # Provides data for InputBoxCallback
+ 
+class JoystickButton(Enum): # Specifies the button on a joystick 
+    ButtonA=AerotechEnsembleStatusNET.JoystickButton.ButtonA #Button A 
+    ButtonB=AerotechEnsembleStatusNET.JoystickButton.ButtonB #Button B 
+    ButtonC=AerotechEnsembleStatusNET.JoystickButton.ButtonC #Button C (both A and B)  
+extend_enum(JoystickButton,'None',getattr(AerotechEnsembleStatusNET.JoystickButton,'None'))
+
+ 
+class JoystickDiagPacket(): # Represents the diagnostic information about the joystick 
+ 
+class NewDiagPacketArrivedEventArgs(): # Provides data for NewDiagPacketArrived
+ 
+class NewTaskStatesArrivedEventArgs(): # Provides data for NewTaskStatesArrived
+ 
+class PlaneStatus(): # Represents plane status
+ 
+class PrintCallbackEventArgs(): # Provides data for PrintCallback
+ 
+class TaskStatesPoller(): # Allows to poll for task state information from a controller in the background  
+
+
+
+'''
     AbsoluteFeedback=None
     AccelerationCommand=None
     AccelerationCommandCounts=None
@@ -107,11 +418,4 @@ class AxisDiagPacket():
         self.VelocityErrorCounts=data.VelocityErrorCounts
         self.VelocityFeedback=data.VelocityFeedback
         self.VelocityFeedbackCounts=data.VelocityFeedbackCounts
-
-
-
-class ErrorInformation():
-    _ErrorInformationNET=None
-    
-    def __init__(self,ErrorInformationNET=AerotechEnsembleStatusNET.ErrorInformation):
-        self._ErrorInformation=ErrorInformationNET
+'''
